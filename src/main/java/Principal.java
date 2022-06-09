e85243dafab55d08833e275933eb89abef63d642
Gabriel Monteiro <gabrielltmonteiro@hotmail.com>
2022-06-09 02:44:45 -0300

    atualização na main para ser compatível com java 1.8

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -5,13 +5,7 @@ public class Principal {
         String opcao = "";
         Calculadora calculadora = new Calculadora();
         while(!opcao.equals("9")){
-            opcao = JOptionPane.showInputDialog("""
-                                                1 - Leitura 
-                                                 2 - Soma 
-                                                 3 - Subtra\u00e7\u00e3o 
-                                                 4 - Produto 
-                                                 5 - Divis\u00e3o 
-                                                 9 - Sair""");
+            opcao = JOptionPane.showInputDialog("1 - Leitura\n 2 - Soma\n 3 - Subtra\u00e7\u00e3o \n 4 - Produto \n 9 - Sair");
             switch (Integer.parseInt(opcao)) {
                 case 1:
                     calculadora.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor A")));
